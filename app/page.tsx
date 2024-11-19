2893c7bf223c978af1ebb8f6b051f4541edc32b2
Mikhail <yugay603@gmail.com>
2024-11-19 16:09:15 -0500

    remove useEffect

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import styles from './page.module.css'
 import Project from './components/projects'
 export default function Home() {
